5fe7293f96b5146b2febb9807b07736f4677c9a1
sachinsmrat123 <36136410+sachinsmrat123@users.noreply.github.com>
2021-06-09 21:40:54 +0530

    initial commit

---
+++ app/src/main/java/com/example/jobedin/LinkedIn.kt
@@ -0,0 +1,11 @@
+package com.example.jobedin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinkedIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linked_in)
+    }
+}
